128ab304dd0d7739b464349e8187dd233a0f2f74
andreneox <andreneox@gmail.com>
2022-08-10 19:10:07 +0000

    Criação de templates HTML e conceito de variaveis.

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask 
+from flask import Flask, render_template
 
-app = Flask("blog")
+app = Flask("hello")
 
-@app.route("/")
-def blog ():
-    return "Blog NNN"
+nomeAula = "Aula python para Web"
+
+@app.route("/usuario")
+def usuario():
+    usuario = [1, "Danilo de Souza Miguel", "Professor"]
+    alunos = ["Andre Guedes", "Lucas Santos", "Alicia Duarte", "Raiane Caroline"]
+    return render_template ("index.html", usuario=usuario, nome=nomeAula, alunos=alunos)
+
+@app.route("/contatos")
+def contato():
+    return render_template("index.html", usuario=usuario, nome=nomeAula)
